d6357ee84cf940285fac029cb81ccf560b5922c5
Przemysław Ren <przemyslaw.ren@gmail.com>
2023-10-31 22:43:14 +0100

    TenantLetter and Tenant change

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
         Person person = new Person("John", "Doe", 123456789, "123 Main Street", "01/01/1970");
 
-        Tenant tenat1 = new Tenant("John", "Doe", 123456789, "123 Main Street", "01/01/1970",);
-        System.out.println(person);
+        Tenant tenat1 = new Tenant("John", "Doe", 123456789, "123 Main Street", "01/01/1970");
+        Tenant tenat2 = new Tenant("Jane", "Doe", 987654321, "321 Main Street", "01/01/1970");
+
+        TenantLetter tenantLetter1 = new TenantLetter(tenat1,"01/01/2020", "Content 1", 1000.0);
+        TenantLetter tenantLetter2 = new TenantLetter(tenat1,"01/01/2020", "Content 2", 2000.0);
+        TenantLetter tenantLetter3 = new TenantLetter(tenat1,"01/01/2020", "Content 3", 2000.0);
+
+        tenat1.addTenantLetter(tenantLetter1);
+        tenat1.addTenantLetter(tenantLetter2);
+        tenat1.addTenantLetter(tenantLetter3);
+
+        System.out.println(tenat1.getTenantLetters());
     }
 }
--- src/TenantLetter.java
+++ src/TenantLetter.java
@@ -1,5 +1,19 @@
 public class TenantLetter {
 
-    String date;
+    private Tenant tenant;
+    private String date;
+    private String content;
+    private double debt;
+
+    public TenantLetter(Tenant tenant, String date, String content, double debt) {
+        this.tenant = tenant;
+        this.date = date;
+        this.content = content;
+        this.debt = debt;
+    }
+
+    public String toString() {
+        return "TenantLetter: " + tenant + ", " + date + ", " + content + ", " + debt;
+    }
 
 }
